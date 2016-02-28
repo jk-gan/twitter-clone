9ce99aa17af754d19ea624bc33d673fc654d7102
Jun Kai <kuhn96@gmail.com>
2016-02-28 13:14:25 +0800

    used react-rails helpers

---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,18 @@
+//= require_tree ./components
+
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>Hello from the component</h1>
+    );
+  }
+}
+
+// let documentReady = () => {
+//   React.render(
+//     <Main/>,
+//     document.getElementById('react')
+//   );
+// };
+//
+// $(documentReady);
